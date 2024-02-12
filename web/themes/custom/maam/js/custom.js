b51bc4c0c4357a295d6fddc73959119aa38ecd29
Muhammad Talha Khan <abdalli17b@gmail.com>
2024-02-12 17:26:45 +0500

    counter animation implementation

--- web/themes/custom/maam/js/custom.js
+++ web/themes/custom/maam/js/custom.js
@@ -18,6 +18,39 @@
           $(navbar).removeClass("scrolling-up");
         }
       });
+
+      document.addEventListener("DOMContentLoaded", function () {
+        const counters = document.querySelectorAll(".counter");
+
+        counters.forEach(function (counter) {
+          const targetValue = parseInt(counter.textContent.trim());
+          startCounter(counter, targetValue);
+        });
+
+        function startCounter(element, targetValue) {
+          let currentValue = 0;
+          const duration = 5000;
+
+          function updateCounter(timestamp) {
+            if (!startTimestamp) startTimestamp = timestamp;
+            const progress = Math.min(
+              (timestamp - startTimestamp) / duration,
+              1
+            );
+            currentValue = Math.ceil(progress * targetValue);
+
+            element.textContent = currentValue;
+
+            if (progress < 1) {
+              window.requestAnimationFrame(updateCounter);
+            }
+          }
+
+          let startTimestamp = null;
+          window.requestAnimationFrame(updateCounter);
+        }
+      });
     },
   };
 })(jQuery, Drupal);
+console.log("hello world");
